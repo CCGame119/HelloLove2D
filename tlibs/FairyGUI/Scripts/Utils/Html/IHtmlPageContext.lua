ed030210959977e7ab20ba01d45a6037e8ad4b1b
chency <chency@gltornado.com>
2018-10-23 15:43:50 +0800

    ADD: IHtmlPageContext

--- tlibs/FairyGUI/Scripts/Utils/Html/IHtmlPageContext.lua
+++ tlibs/FairyGUI/Scripts/Utils/Html/IHtmlPageContext.lua
@@ -9,7 +9,20 @@ local Class = require('libs.Class')
 ---@class Utils.IHtmlPageContext:ClassType
 local IHtmlPageContext = Class.inheritsFrom('IHtmlPageContext')
 
---TODO: Utils.IHtmlPageContext
+---@param owner FairyGUI.RichTextField
+---@param element Utils.HtmlElement
+---@return Utils.IHtmlObject
+function IHtmlPageContext:CreateObject(owner, element) end
+
+---@param obj Utils.IHtmlObject
+function IHtmlPageContext:FreeObject(obj) end
+
+---@param image Utils.HtmlImage
+function IHtmlPageContext:GetImageTexture(image) end
+
+---@param image Utils.HtmlImage
+---@param texture FairyGUI.NTexture
+function IHtmlPageContext:FreeImageTexture(image, texture) end
 
 Utils.IHtmlPageContext = IHtmlPageContext
 return IHtmlPageContext
