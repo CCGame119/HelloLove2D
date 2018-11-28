b031fb2a795fc0e3138b3f7be633fa771e87c0a1
chency <chency@gltornado.com>
2018-11-28 17:16:23 +0800

    ADD: GMovieClip

--- tlibs/FairyGUI/Scripts/UI/GMovieClip.lua
+++ tlibs/FairyGUI/Scripts/UI/GMovieClip.lua
@@ -6,10 +6,13 @@
 
 local Class = require('libs.Class')
 
+local Rect = Love2DEngine.Rect
+
 local GObject = FairyGUI.GObject
 local IAnimationGear = FairyGUI.IAnimationGear
 local IColorGear = FairyGUI.IColorGear
 local EventListener = FairyGUI.EventListener
+local MovieClip = FairyGUI.MovieClip
 
 ---@class FairyGUI.GMovieClip:FairyGUI.GObject @implement IAnimationGear, IColorGear
 ---@field public playing boolean
@@ -30,15 +33,26 @@ function GMovieClip:__ctor()
     self.onPlayEnd = EventListener.new(self, "onPlayEnd")
 end
 
-function GMovieClip:CreateDisplayObject() end
+function GMovieClip:CreateDisplayObject()
+    self._content = MovieClip.new()
+    self._content.gOwner = self
+    self._content.ignoreEngineTimeScale = true
+    self.displayObject = self._content
+end
 
-function GMovieClip:Rewind() end
+function GMovieClip:Rewind()
+    self._content:Rewind()
+end
 
 ---@param anotherMc FairyGUI.MovieClip
-function GMovieClip:SyncStatus(anotherMc) end
+function GMovieClip:SyncStatus(anotherMc)
+    self._content:SyncStatus(anotherMc._content)
+end
 
 ---@param time number
-function GMovieClip:Advance(time) end
+function GMovieClip:Advance(time)
+    self._content:Advance(time)
+end
 
 ---Play from the start to end, repeat times, set to endAt on complete.
 ---从start帧开始，播放到end帧（-1表示结尾），重复times次（0表示无限循环），循环结束后，停止在endAt帧（-1表示参数end）
@@ -46,73 +60,112 @@ function GMovieClip:Advance(time) end
 ---@param End number
 ---@param times number
 ---@param endAt number
-function GMovieClip:SetPlaySettings(start, End, times, endAt) end
-
-function GMovieClip:ConstructFromResource() end
-
-function GMovieClip:Setup_BeforeAdd(buffer, beginPos)
+function GMovieClip:SetPlaySettings(start, End, times, endAt)
+    self.displayObject:SetPlaySettings(start, End, times, endAt)
 end
 
---TODO: FairyGUI.GMovieClip
+function GMovieClip:ConstructFromResource()
+    local packageItem = self.packageItem
+    local _content = self._content
+
+    packageItem:Load()
+
+    self.sourceWidth = packageItem.width
+    self.sourceHeight = packageItem.height
+    self.initWidth = self.sourceWidth
+    self.initHeight = self.sourceHeight
+
+    _content.interval = packageItem.interval
+    _content.swing = packageItem.swing
+    _content.repeatDelay = packageItem.repeatDelay
+    _content:SetData(packageItem.texture, packageItem.frames, Rect(0, 0, self.sourceWidth, self.sourceHeight))
+
+    self:SetSize(self.sourceWidth, self.sourceHeight)
+end
+
+function GMovieClip:Setup_BeforeAdd(buffer, beginPos)
+    GObject.Setup_BeforeAdd(self, buffer, beginPos)
+
+    local _content = self._content
+
+    buffer:Seek(beginPos, 5)
+
+    if (buffer:ReadBool()) then
+        _content.color = buffer:ReadColor()
+        _content.flip = buffer:ReadByte()
+        _content.frame = buffer:ReadInt()
+        _content.playing = buffer:ReadBool()
+    end
+end
+
 
 local __get = Class.init_get(GMovieClip)
 local __set = Class.init_set(GMovieClip)
 
 ---@param self FairyGUI.GMovieClip
-__get.playing  = function(self) end
+__get.playing = function(self) return self._content.playing end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val boolean
-__set.playing  = function(self, val) end
+__set.playing = function(self, val)
+    self._content.playing = val
+    self:UpdateGear(5)
+end
 
 ---@param self FairyGUI.GMovieClip
-__get.frame  = function(self) end
+__get.frame = function(self) return self._content.frame end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val number
-__set.frame  = function(self, val) end
+__set.frame = function(self, val)
+    self._content.frame = val
+    self:UpdateGear(5)
+end
 
 ---@param self FairyGUI.GMovieClip
-__get.color  = function(self) end
+__get.color = function(self) return self._content.color end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val Love2DEngine.Color
-__set.color  = function(self, val) end
+__set.color = function(self, val)
+    self._content.color = val
+    self:UpdateGear(4)
+end
 
 ---@param self FairyGUI.GMovieClip
-__get.flip  = function(self) end
+__get.flip = function(self) return self._content.flip end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val FairyGUI.FlipType
-__set.flip  = function(self, val) end
+__set.flip = function(self, val) self._content.flip = val end
 
 ---@param self FairyGUI.GMovieClip
-__get.material  = function(self) end
+__get.material = function(self) return self._content.material end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val Love2DEngine.Material
-__set.material  = function(self, val) end
+__set.material = function(self, val) self._content.material = val end
 
 ---@param self FairyGUI.GMovieClip
-__get.shader  = function(self) end
+__get.shader = function(self) return self._content.shader end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val string
-__set.shader  = function(self, val) end
+__set.shader = function(self, val) self._content.shader = val end
 
 ---@param self FairyGUI.GMovieClip
-__get.timeScale  = function(self) end
+__get.timeScale = function(self) return self._content.timeScale  end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val number
-__set.timeScale  = function(self, val) end
+__set.timeScale = function(self, val) self._content.timeScale = val end
 
 ---@param self FairyGUI.GMovieClip
-__get.ignoreEngineTimeScale  = function(self) end
+__get.ignoreEngineTimeScale = function(self) return _content.ignoreEngineTimeScale end
 
 ---@param self FairyGUI.GMovieClip
 ---@param val boolean
-__set.ignoreEngineTimeScale  = function(self, val) end
+__set.ignoreEngineTimeScale = function(self, val) self._content.ignoreEngineTimeScale = val  end
 
 
 FairyGUI.GMovieClip = GMovieClip
